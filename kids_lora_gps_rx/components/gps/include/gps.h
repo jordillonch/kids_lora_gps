de734d6930ae723394ad80b487afc0f58a294003
Jordi Llonch <llonch.jordi@gmail.com>
2022-08-20 11:00:42 +0200

    receiver code

--- kids_lora_gps_rx/components/gps/include/gps.h
+++
@@ -1,9 +0,0 @@
-#include <esp_err.h>
-#include "types.h"
-#include "nmea_parser.h"
-
-nmea_parser_handle_t gps_init(esp_err_t *result);
-
-esp_err_t gps_deinit(nmea_parser_handle_t handler);
-
-esp_err_t gps_add_handler(nmea_parser_handle_t nmea_hdl, esp_event_handler_t event_handler, void *handler_args);
--- kids_lora_gps_rx/main/kids_lora_gps_rx.c
+++ kids_lora_gps_rx/main/kids_lora_gps_rx.c
@@ -3,146 +3,62 @@
 #include <freertos/task.h>
 #include <esp_log.h>
 #include <cbor.h>
-#include <time.h>
-#include "gps.h"
+#include <esp_check.h>
 #include "lvgl.h"
 #include "display.h"
 #include "lora.h"
 
-#define TAG "kids_lora_gps"
-#define LORA_SEND_GPS_COORDINATES_EVERY_SECONDS 60
+#define TAG "kids_lora_gps_rx"
 
 static lv_obj_t *scr;
 static lv_obj_t *label_gps_position;
-static lv_obj_t *label_lora_transmission;
-static int lora_packet_count;
-static time_t lora_sent_last_time;
-
-static nmea_parser_handle_t init_gps();
-
-static void on_gps_update_screen(void *event_handler_arg,
-                                 esp_event_base_t event_base,
-                                 int32_t event_id,
-                                 void *event_data);
-
-static void on_gps_send_position_through_lora(void *event_handler_arg,
-                                              esp_event_base_t event_base,
-                                              int32_t event_id,
-                                              void *event_data);
+static lv_obj_t *label_lora_receive;
 
 static void init_screen();
 
 static void init_lora();
 
+static void task_lora_rx(void *p);
+
+static esp_err_t parse_incoming_lora_message(uint8_t *buf, float *latitude, float *longitude, float *speed);
+
 void app_main(void) {
-  nmea_parser_handle_t gps = init_gps();
   display_init(init_screen);
   init_lora();
-
-  gps_add_handler(gps, on_gps_update_screen, NULL);
-  gps_add_handler(gps, on_gps_send_position_through_lora, NULL);
+  xTaskCreate(&task_lora_rx, "task_lora_rx", 2048, NULL, 5, NULL);
 
   while (true) {
     vTaskDelay(10000 / portTICK_PERIOD_MS);
   }
-
-  gps_deinit(gps);
 }
 
-static void on_gps_update_screen(void *event_handler_arg,
-                                 esp_event_base_t event_base, int32_t
-                                 event_id,
-                                 void *event_data) {
-  gps_t *gps = NULL;
-  switch (event_id) {
-    case GPS_UPDATE:
-      gps = (gps_t *) event_data;
-      char buffer[100];
-      sprintf(buffer, "Ariadna & Julia position: "
-                      "lat=%.05f°N | "
-                      "lng=%.05f°E",
-                      gps->latitude, gps->longitude);
-      ESP_LOGD(TAG, "%s", buffer);
+static void task_lora_rx(void *p) {
+  uint8_t buf[50];
+  float latitude, longitude, speed;
+  int x;
+  for (;;) {
+    lora_receive();    // put into receive mode
 
-      sprintf(buffer, "Ariadna & Julia position:\n"
-                      "lat=%.05f°N\n"
-                      "lng=%.05f°E",
-                      gps->latitude, gps->longitude);
-      lv_label_set_text(label_gps_position, buffer);
-      lv_obj_align(label_gps_position, NULL, LV_ALIGN_IN_BOTTOM_LEFT, 3, -3);
-      break;
-    case GPS_UNKNOWN:
-      /* print unknown statements */
-      ESP_LOGW(TAG, "Unknown statement:%s", (char *) event_data);
-      break;
-    default:
-      break;
-  }
-}
+    while (lora_received()) {
+      x = lora_receive_packet(buf, sizeof(buf));
+      buf[x] = 0;
 
-static void on_gps_send_position_through_lora(void *event_handler_arg,
-                                              esp_event_base_t event_base,
-                                              int32_t event_id,
-                                              void *event_data) {
-  time_t now;
-  gps_t *gps = NULL;
-  switch (event_id) {
-    case GPS_UPDATE:
-      time(&now);
-      if (difftime(now, lora_sent_last_time) >= LORA_SEND_GPS_COORDINATES_EVERY_SECONDS) {
-        gps = (gps_t *) event_data;
-        //  Max output size is 50 bytes (which fits in a LoRa packet)
-        uint8_t output[50];
-        CborEncoder root_encoder, map_encoder;
-        cbor_encoder_init(&root_encoder, output, sizeof(output), 0);
-        CborError res = cbor_encoder_create_map(&root_encoder, &map_encoder, 3);
-        ESP_ERROR_CHECK(res);
-        res = cbor_encode_text_stringz(&map_encoder, "lat");
-        ESP_ERROR_CHECK(res);
-        res = cbor_encode_float(&map_encoder, gps->latitude);
-        ESP_ERROR_CHECK(res);
-        res = cbor_encode_text_stringz(&map_encoder, "lng");
-        ESP_ERROR_CHECK(res);
-        res = cbor_encode_float(&map_encoder, gps->longitude);
-        ESP_ERROR_CHECK(res);
-        res = cbor_encode_text_stringz(&map_encoder, "s");
-        ESP_ERROR_CHECK(res);
-        res = cbor_encode_float(&map_encoder, gps->speed);
-        ESP_ERROR_CHECK(res);
-        res = cbor_encoder_close_container(&root_encoder, &map_encoder);
-        ESP_ERROR_CHECK(res);
-        size_t output_len = cbor_encoder_get_buffer_size(&root_encoder, output);
-        ESP_LOGD(TAG, "CBOR Output: %d bytes", output_len);
-        for (int i = 0; i < output_len; i++) {
-          ESP_LOGD(TAG, "0x%02x", output[i]);
-        }
-
-        lora_send_packet(output, (int) output_len);
-        lora_packet_count++;
-
-        char buffer[30];
-        sprintf(buffer, "LORA pkt: %i", lora_packet_count);
-        lv_label_set_text(label_lora_transmission, buffer);
-        lv_obj_align(label_lora_transmission, NULL, LV_ALIGN_IN_TOP_LEFT, 3, 3);
-
-        time(&lora_sent_last_time);
+      if (ESP_OK == parse_incoming_lora_message(buf, &latitude, &longitude, &speed)) {
+        char buffer[100];
+        sprintf(buffer, "Ariadna & Julia position:\n"
+                        "lat=%.05f°N\n"
+                        "lng=%.05f°E\n"
+                        "spd=%.02f m/s",
+                latitude, longitude, speed);
+        ESP_LOGD(TAG, "%s", buffer);
+        lv_label_set_text(label_gps_position, buffer);
+        lv_obj_align(label_gps_position, NULL, LV_ALIGN_IN_BOTTOM_LEFT, 3, -3);
       }
-      break;
-    case GPS_UNKNOWN:
-      /* print unknown statements */
-      ESP_LOGW(TAG, "Unknown statement:%s", (char *) event_data);
-      break;
-    default:
-      break;
-  }
-}
 
-static nmea_parser_handle_t init_gps() {
-  esp_err_t result;
-  nmea_parser_handle_t gps;
-  gps = gps_init(&result);
-  ESP_ERROR_CHECK(result);
-  return gps;
+      lora_receive();
+    }
+    vTaskDelay(1);
+  }
 }
 
 static void init_screen() {
@@ -150,16 +66,66 @@ static void init_screen() {
   label_gps_position = lv_label_create(scr, NULL);
   lv_label_set_text(label_gps_position, "");
 
-  label_lora_transmission = lv_label_create(scr, NULL);
-  lv_label_set_text(label_lora_transmission, "LORA pkt: 0");
+  label_lora_receive = lv_label_create(scr, NULL);
+  lv_label_set_text(label_lora_receive, "LORA pkt: 0");
   static lv_style_t style;
   lv_style_init(&style);
   lv_style_set_text_font(&style, LV_STATE_DEFAULT, &lv_font_montserrat_14);
-  lv_obj_add_style(label_lora_transmission, LV_OBJ_PART_MAIN, &style);
+  lv_obj_add_style(label_lora_receive, LV_OBJ_PART_MAIN, &style);
 }
 
 static void init_lora() {
   lora_init();
   lora_set_frequency(868e6);
   lora_enable_crc();
+}
+
+static esp_err_t lora_message_get_float(CborValue *it, float *value);
+
+static esp_err_t parse_incoming_lora_message(uint8_t *buf, float *latitude, float *longitude, float *speed) {
+  CborError ret;
+  CborValue it;
+  CborParser root_parser;
+  CborType type;
+  CborValue recursed;
+
+  cbor_parser_init(buf, sizeof(buf), 0, &root_parser, &it);
+  type = cbor_value_get_type(&it);
+  if (type != CborMapType) {
+    ESP_LOGE(TAG, "Invalid type: 0x%04X", type);
+    return ESP_ERR_INVALID_ARG;
+  }
+
+  ret = cbor_value_enter_container(&it, &recursed);
+  if (ret != CborNoError) {
+    ESP_LOGE(TAG, "%s(%d)", __FUNCTION__, __LINE__);
+    return ESP_ERR_INVALID_ARG;
+  }
+
+  ESP_RETURN_ON_ERROR(lora_message_get_float(&it, latitude), TAG, "unable to parse latitude");
+  ESP_RETURN_ON_ERROR(lora_message_get_float(&it, longitude), TAG, "unable to parse longitude");
+  ESP_RETURN_ON_ERROR(lora_message_get_float(&it, speed), TAG, "unable to parse speed");
+
+  ret = cbor_value_leave_container(&it, &recursed);
+  if (ret != CborNoError) {
+    ESP_LOGE(TAG, "%s(%d)", __FUNCTION__, __LINE__);
+    return ESP_ERR_INVALID_ARG;
+  }
+
+  return ESP_OK;
+}
+
+static esp_err_t lora_message_get_float(CborValue *it, float *value) {
+  CborError ret;
+  CborType type;
+  type = cbor_value_get_type(it);
+  if (type != CborFloatType) {
+    ESP_LOGE(TAG, "Invalid type: 0x%04X", type);
+    return ESP_ERR_INVALID_ARG;
+  }
+  ret = cbor_value_get_float(it, value);
+  if (ret != CborNoError) {
+    return ESP_ERR_INVALID_ARG;
+  }
+  return ESP_OK;
 }
